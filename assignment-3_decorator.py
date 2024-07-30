f6d61a61ce300d0177a0ecf71a07c294bedf4f0c
tharani2503 <vinothdhanavel2503@gmail.com>
2024-07-31 00:45:13 +0530

    Add files via upload

---
+++ assignment-3_decorator.py
@@ -0,0 +1,36 @@
+##def gift():
+##    print("transaction successful")
+####    return gift
+
+def shopkeeper(fp):
+    def wrap(*args,**kwds):
+        import time
+        start=time.time()
+        i=fp(*args,**kwds)
+        end=time.time()
+        print("time taken for transaction",end-start)
+        return i
+    
+    return wrap
+def shopkeeper2(fp):
+    def wrap(*args,**kwds):
+        print("Function started")
+        i=fp(*args,**kwds)
+        print('function ended')
+        return i
+    
+    return wrap
+
+        
+@shopkeeper2
+@shopkeeper
+def funcall(a):
+    if a>1000:
+        print("transaction done")
+    else:
+        print("transaction failed")
+            
+
+
+
+    
